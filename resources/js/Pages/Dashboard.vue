84675a4e9de2697ff49385e32c98efc73ecd9f08
ronitm1 <121118391+ronitm1@users.noreply.github.com>
2023-02-21 00:30:19 +0100

    learning Vue with Inertia

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,35 @@
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+              Hello ,{{ name }}
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <Welcome />
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
+<script>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import Welcome from '@/Components/Welcome.vue';
+export default {
+    name: 'Dashboard',
+    components: {
+        AppLayout,
+        Welcome, 
+    },
+    props: {
+        name: String,
+        frameworks: Array,
+    }
+}
+</script>
+<style lang="">
+    
+</style>
