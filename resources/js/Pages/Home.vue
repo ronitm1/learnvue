dd01c5f3c67cc53b3a778b0757e65a95464efcce
ronitm1 <121118391+ronitm1@users.noreply.github.com>
2023-02-26 17:33:56 +0100

    changes

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -6,8 +6,9 @@
         
         <p>In this series, we will use the following frameworks</p>
         
-
-        <button class="bg-blue-400 2xl:bg-green-500 py-2  px-10 hover:bg-green-400 font-bold hover:font-thin xs:">Submit</button>
+        </div>      
+<br>
+        <div>
         <ul>
             <li v-for="framework of frameworks" v-text="framework"></li>
         </ul>
